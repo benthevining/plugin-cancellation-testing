6c8105b7a205aefce2ff8fa446360a5793a43505
Ben Vining <ben.the.vining@gmail.com>
2024-01-23 23:53:48 -0600

    refactoring

--- config.cmake
+++ config.cmake
@@ -4,21 +4,13 @@ include ("${CMAKE_CURRENT_LIST_DIR}/PluginCancellationTargets.cmake")
 
 include ("${CMAKE_CURRENT_LIST_DIR}/CancellationTesting.cmake")
 
-set (cancellation_find_package_name "${CMAKE_FIND_PACKAGE_NAME}")
-
 include (FeatureSummary)
 
 set_package_properties (
-	"${cancellation_find_package_name}" 
+	"${CMAKE_FIND_PACKAGE_NAME}" 
 	PROPERTIES 
 		URL "@plugin-cancellation_HOMEPAGE_URL@"
 		DESCRIPTION "@plugin-cancellation_DESCRIPTION@"
 )
 
-include (CMakeFindDependencyMacro)
-
-find_dependency (JUCE 7.0.9)
-
-check_required_components ("${cancellation_find_package_name}")
-
-unset (cancellation_find_package_name)
+check_required_components ("${CMAKE_FIND_PACKAGE_NAME}")
